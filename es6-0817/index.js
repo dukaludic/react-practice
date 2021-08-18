bf52e2198552587b7f96d7476d0be745afe6e64e
dukaludic <dukaludic@gmail.com>
2021-08-18 15:51:37 +0200

    0818

--- es6-0817/index.js
+++ es6-0817/index.js
@@ -1,58 +1,183 @@
-'use strict';
+"use strict";
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-  order: function(starterIndex, mainIndex) {
-      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
   },
-
-  orderDelivery: function({starterIndex = 1, mainIndex = 0, time, address}) {
-    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
   },
-
-  orderPasta: function(ing1, ing2, ing3) {
-    console.log(`Here is your delicious pasta with ${ing1} ${ing2} ${ing3}`)
-  },
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
   },
 };
 
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  //ES6 enhanced object literal
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1} ${ing2} ${ing3}`);
+  },
+
+  orderPizza(mainIngredient, ...otherIng) {
+    console.log(mainIngredient);
+    console.log(otherIng);
+  },
+};
+
+// Property names
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+// Property values
+const values = Object.values(openingHours);
+console.log(values);
+
+// entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
+// //OPTIONAL chaining 2020 //////////////////////
+// if (restaurant.openingHours && restaurant.openingHours.fri.open) {
+//   console.log(restaurant.openingHours.fri.open);
+// }
+
+// console.log(restaurant.openingHours.mon?.open);
+// //example
+// const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+// for (const day of days) {
+//   console.log(day);
+//   const open = restaurant.openingHours[day]?.open ?? "closed";
+//   console.log(`On ${day} is opened at ${open}`);
+// }
+
+// // methods
+// console.log(restaurant.order?.(0, 1) ?? "Method doesn't exist");
+
+// // arrays
+// const users = [{ name: "Jonas", email: "hello@jonas.io" }];
+// console.log(users[0]?.email ?? "User doesn't exist");
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
+
+// console.log([...menu.entries()]);
+
+// for (const )
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// // Nullish: null and undefined (no other)
+// const guestsCorrect = restaurant.numGuests ?? 10;
+// console.log(guestsCorrect);
+
+//Short circuiting use any data type, return any data type
+// console.log("---------OR-------");
+// console.log(3 || "Jonas"); //3
+// console.log("" || "Jonas"); // Jonas
+// console.log(true || 0); // true
+// console.log(undefined || null); //null
+
+// console.log(undefined || 0 || "" || "Hello" || 23 || null);
+
+// restaurant.numGuests = 23;
+// const guests = restaurant.numGuests ? restaurant.numGuests : 10;
+
+// console.log(guests);
+
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// console.log("---------AND-------");
+// console.log(0 && "Jonas");
+// console.log(7 && "Jonas");
+
+// console.log("hello" && 23 && null && "Jonas");
+
+// // if (restaurant.orderPizza) {
+// //   restaurant.orderPizza("mushrooms", "spinach");
+// // }
+
+// // execute code from 2nd operand
+// restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
+
+// functions
+
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// };
+
+// add(2, 3);
+// add(5, 3, 7, 2);
+
+// const x = [23, 5, 7];
+// add(...x);
+
+// restaurant.orderPizza("mushrooms", "olives", "onions", "spinach");
+// restaurant.orderPizza("mushrooms");
+
 // REST
 //spread because on the right side of =
-const arr = [1, 2, ...[3, 4]]
+// const arr = [1, 2, ...[3, 4]]
 
-// Rest because on the left side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5]
-console.log(a, b, others)
+// // Rest because on the left side of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5]
+// console.log(a, b, others)
 
-const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+// const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
 
-console.log(pizza, risotto, otherFood);
-
-
+// console.log(pizza, risotto, otherFood);
 
 //  Spread Objects
 // const newRestaurant = {foundedIb: 1995, ...restaurant, founder: 'Guiseppe'}
@@ -106,7 +231,6 @@ console.log(pizza, risotto, otherFood);
 //     starterIndex: 2
 // });
 
-
 // const {name, openingHours,  categories} = restaurant;
 
 // console.log(name, openingHours,  categories);
@@ -131,10 +255,6 @@ console.log(pizza, risotto, otherFood);
 // const {fri: {open: o, close: c}} = openingHours;
 // console.log(o, c)
 
-
-
-
-
 // const arr = [2,3,4]
 // const a = arr[0];
 // const b = arr[1];
@@ -145,7 +265,6 @@ console.log(pizza, risotto, otherFood);
 
 // let [main, ,secondary] = restaurant.categories;
 // console.log(main, secondary);
-
 
 // switching variables
 // const temp = main;
